0625a04f7b031d9c32c98384086efc380a72710d
henriqueTsugiyama <henrique-tsu@hotmail.com>
2021-03-19 17:00:12 -0300

    RecyclerView setup that'll show memory cards.

--- app/src/main/java/com/example/mymemorygame/MainActivity.kt
+++ app/src/main/java/com/example/mymemorygame/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.mymemorygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
 import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -17,5 +19,9 @@ class MainActivity : AppCompatActivity() {
         rvBoard = findViewById(R.id.rvBoard)
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
+
+        rvBoard.adapter = MemoryBoardAdapter(this, 8)
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager = GridLayoutManager(this, 2)
     }
 }
