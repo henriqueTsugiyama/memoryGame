1bb991908e45aa17dbc83972c62eb7f353f7dfb9
henriqueTsugiyama <henrique-tsu@hotmail.com>
2021-03-23 13:10:23 -0300

    Packages created: models & utils. Models for number of cards that increase difficulty and utils for icons list reference

---
+++ app/src/main/java/com/example/mymemorygame/utils/Constants.kt
@@ -0,0 +1,18 @@
+package com.example.mymemorygame.utils
+
+import com.example.mymemorygame.R
+
+val DEFAULT_ICONS = listOf(
+        R.drawable.ic_bar,
+        R.drawable.ic_baseball,
+        R.drawable.ic_basketball,
+        R.drawable.ic_esports,
+        R.drawable.ic_football,
+        R.drawable.ic_handball,
+        R.drawable.ic_hockey,
+        R.drawable.ic_mma,
+        R.drawable.ic_soccer,
+        R.drawable.ic_star,
+        R.drawable.ic_tennis,
+        R.drawable.ic_volleyball
+)
